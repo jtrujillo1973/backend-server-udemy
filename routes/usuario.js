af1d0887fdb82eda6ff836fb867267c7fdead0c6
Javier Trujillo <jtrujillo1973@gmail.com>
2019-06-06 00:52:00 -0500

    Seccion 13 terminada

--- routes/usuario.js
+++ routes/usuario.js
@@ -102,8 +102,8 @@ app.put('/:id', mdAutenticacion.verificaToken, (req, res) => {
 // ==========================================
 // Crear un nuevo usuario
 // ==========================================
-app.post('/', mdAutenticacion.verificaToken, (req, res) => {
-
+// app.post('/', mdAutenticacion.verificaToken, (req, res) => {
+app.post('/', (req, res) => {
     var body = req.body;
 
     var usuario = new Usuario({
